4e621a6b7c135eba57eec6ce42141c3c3efecfc0
Carlos Peralta <carlos9917@gmail.com>
2021-11-03 10:00:34 +0000

    Renamed transfer script

--- transfer/check_daily.sh
+++ transfer/check_daily.sh
@@ -3,24 +3,38 @@ cd /home/ms/dk/nhd/scripts/harmonie_harp/transfer
 py38=/hpc/perm/ms/dk/nhd/miniconda3/envs/py38/bin/python
 MODEL=EC9
 
-$py38 ./check_new_plot.py
 
+function transfer_all_figs()
+{
 #Copying over the figures to hirlam
 for PNG in `ls -1 *png`; do
  scp -p $PNG cperalta@hirlam.org:/data/portal/uwc_west/figs/
  chmod 755 $PNG
- #scorecards_2021090700_20210927.png 
- FPRE=`echo $PNG | awk '{print substr($1,1,10)}'`
- if [ $FPRE == "scorecards" ]; then
-  DATE1=`echo $PNG | awk -F"_" '{print $2}'`
-  DATE2=`echo $PNG | awk -F"_" '{print $3}' | awk -F"." '{print $1}'`
- fi
- echo $DATE1 $DATE2
+ #FPRE=`echo $PNG | awk '{print substr($1,1,10)}'`
+ #if [ $FPRE == "scorecards" ]; then
+ # DATE1=`echo $PNG | awk -F"_" '{print $2}'`
+ # DATE2=`echo $PNG | awk -F"_" '{print $3}' | awk -F"." '{print $1}'`
+ #fi
+ #echo $DATE1 $DATE2
 done
+}
 
 DATE1=2021100100
 DATE2=2021101023
+if [[ -z $1 ]] && [[ -z $2 ]]; then
+    echo "Please provide init and final date in YYYYMMDDHH format"
+    exit 1
+else
+    DATE1=$1
+    DATE2=$2
+fi
 
+#Copy plots from duuw, or wherever they were generated
+# (paths hardcoded in script for the moment)
+$py38 ./get_new_plots.py
+
+#Transfer all files to hirlam
+transfer_all_figs
 
 #Generate modified html:
 cd simple_web
@@ -33,10 +47,9 @@ echo "Transferring updated html"
 chmod 755 html/scorecards.html
 chmod 755 html/scores.html
 chmod 755 html/*.html
+
 cd html
 for HTML in `ls *.html`;do
-scp -p $HTML cperalta@hirlam.org:/data/portal/uwc_west
-#scp -p html/scorecards.html cperalta@hirlam.org:/data/portal/uwc_west
-#scp -p html/scores.html cperalta@hirlam.org:/data/portal/uwc_west
+  scp -p $HTML cperalta@hirlam.org:/data/portal/uwc_west
 done
 cd -
