eb658ee609b40ad543446d631b9d6c412f39bb9d
Carlos Peralta <carlos9917@gmail.com>
2022-01-05 14:26:56 +0000

    script to copy figs and html files to hirlam website

---
+++ transfer/copysimweb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#NOTE: this should be run fro nhd, not duuw
+
+cd /home/ms/dk/nhd/R/harmonie_harp/transfer
+
+END=`date --date "1 days ago" +'%Y%m%d'`
+YYYYMM=`date +'%Y%m'`
+BEG=${YYYYMM}0100
+./copy2hirlam.sh $BEG $END
+
