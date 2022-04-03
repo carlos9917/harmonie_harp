c70315f15b0edd9d7ff53ec6891c3cc8d7afa1d4
Carlos Peralta <carlos9917@gmail.com>
2022-04-03 17:24:56 +0000

    nhd: copy tarball to hirlam before copying the rest of the files

--- transfer/copysimweb.sh
+++ transfer/copysimweb.sh
@@ -3,6 +3,11 @@
 
 cd /home/ms/dk/nhd/R/harmonie_harp/transfer
 
+#Send the verif scores rds
+SHINYPATH=/data4/portal/harp/uwc_west
+TARBALL=/scratch/ms/ie/duuw/vfld_vobs_sample/verif_scores/verif_scores.tar.gz
+echo "Sending $TARBALL to HIRLAM: $SHINYPATH"
+scp -p $TARBALL cperalta@hirlam.org:$SHINYPATH
 
 if [[ -z $1 ]] && [[ -z $2 ]]; then
     YDAY=`date --date "1 days ago" +'%Y%m%d'`
@@ -16,8 +21,3 @@ else
     ./copy2hirlam.sh $BEG $END
 fi
 
-#Send the verif scores rds
-SHINYPATH=/data4/portal/harp/uwc_west
-TARBALL=/scratch/ms/ie/duuw/vfld_vobs_sample/verif_scores/verif_scores.tar.gz
-echo "Sending $TARBALL to HIRLAM: $SHINYPATH"
-scp -p $TARBALL cperalta@hirlam.org:$SHINYPATH
